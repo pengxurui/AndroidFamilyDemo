3c9bea1bfb052a720425ec05c74bcd37493b2044
pengxurui <pengxr@91160.com>
2021-05-15 21:36:36 +0800

    feature(MavenPublish)

---
+++ MavenPublish/lib/src/main/java/com/xurui/lib/PublishActivity.kt
@@ -0,0 +1,11 @@
+package com.xurui.lib
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PublishActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_publish)
+    }
+}
