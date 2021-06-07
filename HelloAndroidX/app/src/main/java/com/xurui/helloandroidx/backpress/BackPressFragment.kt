cd85f99164d28b1fb55d41e2a34f6b4101401b45
pengxurui <pengxr@91160.com>
2021-06-07 13:07:51 +0800

    feat(HelloAndroidX):OnBackPressedDispatcher

--- HelloAndroidX/app/src/main/java/com/xurui/helloandroidx/backpress/BackPressFragment.kt
+++ HelloAndroidX/app/src/main/java/com/xurui/helloandroidx/backpress/BackPressFragment.kt
@@ -2,6 +2,8 @@ package com.xurui.helloandroidx.backpress
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import com.xurui.helloandroidx.R
 import com.xurui.helloandroidx.databinding.FragmentBackpressBinding
@@ -13,9 +15,21 @@ import com.xurui.ktx.viewBinding
  */
 class BackPressFragment : Fragment(R.layout.fragment_backpress) {
 
+    private val dispatcher by lazy { requireActivity().onBackPressedDispatcher }
+
     var text: String by fragmentArgument()
 
     private val binding by viewBinding(FragmentBackpressBinding::bind)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        dispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                Toast.makeText(context, "BackPressFragment - handleOnBackPressed", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
